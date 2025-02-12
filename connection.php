6a14f1fb3888619359f52c34b89f5070545e092e
Zenji <zenjiyangco@gmail.com>
2025-02-13 04:14:28 +0800

    sweet alert

--- connection.php
+++ connection.php
@@ -3,13 +3,13 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "ojt";
+$port = 3307;
 
 //connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 // echo "Connected successfully";
-?>
